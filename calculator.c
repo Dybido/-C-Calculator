7034caa0d67938804236835c69040bc4e3fd0601
Dybido <dybido93@gmail.com>
2016-06-29 22:25:05 +1000

    Initial commit

---
+++ calculator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <math.h> 
+
+int main(int argc, char *argv[]){
+	printf("Please enter two numbers ")
+	return 0;
+}
+
+int addition(){
+
+}
+
+int subtraction(){
+
+}
+
+int multiply(){
+
+}
+
+int division(){
+
+}
