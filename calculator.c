bf896a122e5f191cac74ff0e76d386a1152b4b18
Dybido <dybido93@gmail.com>
2016-06-29 23:05:32 +1000

    +Implemented basic arithmetics

--- calculator.c
+++ calculator.c
@@ -2,23 +2,62 @@
 #include <stdlib.h> 
 #include <math.h> 
 
+void addition(double a, double b);
+void subtraction(double a, double b);
+void multiply(double a, double b);
+void division(double a, double b);
+
 int main(int argc, char *argv[]){
-	printf("Please enter two numbers ")
+	double a, b;
+	char operator;
+	printf("Please enter two numbers \n");
+	if(scanf("%lf %lf", &a, &b) > 0){
+		;
+	}
+	printf("The numbers you entered are %.1lf %.1lf\n", a, b);
+
+	printf("Please enter an operation \n");
+	if(scanf(" %c", &operator) > 0){
+		;
+	}
+	printf("The operation you entered is %c\n", operator);
+
+	if(operator == '+'){
+		addition(a, b);
+	}else if(operator == '-'){
+		subtraction(a, b);
+	}else if(operator == '*'){
+		multiply(a, b);
+	}else if(operator == '/'){
+		division(a, b);
+	}else{
+		printf("Operation is unknown \n");
+		exit(0);
+	}
 	return 0;
 }
 
-int addition(){
-
+void addition(double a, double b){
+	double c = a + b;
+	printf("The result of %.1lf + %.1lf is %.1lf\n", a, b, c);
 }
 
-int subtraction(){
-
+void subtraction(double a, double b){
+	double c = a - b;
+	printf("The result of %.1lf - %.1lf is %.1lf\n", a, b, c);
 }
 
-int multiply(){
-
+void multiply(double a, double b){
+	double c = a * b;
+	printf("The result of %.1lf * %.1lf is %.1lf\n", a, b, c);
 }
 
-int division(){
+void division(double a, double b){
+	if(b == 0){
+		printf("Division by zero, world's end\n");
+		exit(0);
+	}
+	double c = a / b;
 
+	printf("The result of %.1lf / %.1lf is %.1lf\n", a, b, c); 
 }
